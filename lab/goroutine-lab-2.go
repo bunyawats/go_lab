3c16e8c38ea5a764e43f508b0c9a321d21b507c2
Bunyawat Singchai <bunyawat.s@gmail.com>
2022-02-01 17:45:50 +0700

    add GoRountineLab2()

---
+++ lab/goroutine-lab-2.go
@@ -0,0 +1,36 @@
+package lab
+
+import (
+	"fmt"
+	"runtime"
+	"sync"
+)
+
+func GoRountineLab2() {
+	x := 10
+	y := 10
+	c := make(chan int)
+
+	var wg sync.WaitGroup
+
+	go func() {
+		for i := 0; i < x; i++ {
+			wg.Add(1)
+			go func(m int) {
+				for j := 0; j < y; j++ {
+					c <- j + (10 * m)
+				}
+				wg.Done()
+			}(i)
+			fmt.Println("ROUTINES", runtime.NumGoroutine())
+
+		}
+		wg.Wait()
+		close(c)
+	}()
+
+	for number := range c {
+		fmt.Println(number)
+	}
+	fmt.Println("ROUTINES", runtime.NumGoroutine())
+}
